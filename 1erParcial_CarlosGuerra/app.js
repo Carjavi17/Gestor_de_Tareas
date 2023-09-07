ae76a03efbcf762d851c332199416035a4d8ed63
Carjavi17 <carlosjguerrab17@gmail.com>
2023-09-07 01:16:25 -0300

    1er Parcial de Programacion III

---
+++ 1erParcial_CarlosGuerra/app.js
@@ -0,0 +1,75 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        tasks: tasks,
+        newTask: '',
+        searchQuery: '',
+        filteredPendingTasks: [],
+        filteredCompletedTasks: [],
+    },
+    computed: {
+        pendingTasks() {
+            return this.tasks.filter(task => !task.completed);
+        },
+        completedTasks() {
+            return this.tasks.filter(task => task.completed);
+        },
+    },
+    methods: {
+        addTask() {
+            if (this.newTask.trim() !== '') {
+                const newTask = {
+                    name: this.newTask,
+                    completed: false
+                };
+                this.tasks.push(newTask);
+                this.newTask = '';
+        
+                this.filteredPendingTasks = this.tasks.filter(task => !task.completed);
+                this.filteredCompletedTasks = this.tasks.filter(task => task.completed);
+            }
+        },
+        
+        toggleTaskStatus(task) {
+            task.completed = !task.completed;
+            this.filteredPendingTasks = this.tasks.filter(task => !task.completed);
+            this.filteredCompletedTasks = this.tasks.filter(task => task.completed);
+        },
+        deleteTask(taskToDelete) {
+            const index = this.tasks.indexOf(taskToDelete);
+            if (index > -1) {
+                this.tasks.splice(index, 1);
+            }
+            this.filteredPendingTasks = this.tasks.filter(task => !task.completed);
+            this.filteredCompletedTasks = this.tasks.filter(task => task.completed);
+        },
+        editTask(task) {
+            task.editing = true;
+        },
+        
+        saveTask(task) {
+            task.editing = false;
+        },
+        filterTasks() {
+            const searchTerm = this.searchQuery.toLowerCase();
+            if (!searchTerm) {
+                this.filteredPendingTasks = this.tasks.filter(task => !task.completed);
+                this.filteredCompletedTasks = this.tasks.filter(task => task.completed);
+            } else {
+                this.filteredPendingTasks = this.tasks.filter(task =>
+                    task.name.toLowerCase().includes(searchTerm) && !task.completed
+                );
+                this.filteredCompletedTasks = this.tasks.filter(task =>
+                    task.name.toLowerCase().includes(searchTerm) && task.completed
+                );
+            }
+        },             
+    },
+    created() {
+        this.filteredPendingTasks = this.tasks.filter(task => !task.completed);
+        this.filteredCompletedTasks = this.tasks.filter(task => task.completed);
+    }
+});
+
+
+
---
+++ 1erParcial_CarlosGuerra/data.js
@@ -0,0 +1,17 @@
+const tasks = [
+    {
+        name: 'Mirar una serie',
+        completed: true,
+        editing: false
+    },
+    {
+        name: 'Ir al cine',
+        completed: false,
+        editing: false
+    },
+    {
+        name: 'Hacer ejercicios',
+        completed: true,
+        editing: false
+    }
+];
